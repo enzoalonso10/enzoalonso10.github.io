ecbc2cc9d7efdb91619629d0029a150f2cfffc77
Enzo Alonso <110264593+enzoalonso10@users.noreply.github.com>
2023-01-28 11:40:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let menuVisible = false;
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    document.getElementById("nav").classList ="";
+    menuVisible = false;
+}
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("html");
+        habilidades[2].classList.add("css");
+        habilidades[3].classList.add("typescript");
+        habilidades[4].classList.add("sql");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
+    }
+}
+
+window.onscroll = function(){
+    efectoHabilidades();
+}
